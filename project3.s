b3f14b516a8b3fa7a47b12d3c81668649074931f
Emmanuel Fowora <43800038+Fowora@users.noreply.github.com>
2019-11-28 21:28:04 -0500

    a function to check each character type

--- project3.s
+++ project3.s
@@ -48,3 +48,17 @@ loop:
 	beq $s0, 10, next  	
 	addi $t0,$t0,1 	
 	beq $s0, 44, substring 	
+	
+
+check_characters:
+	#checks each character
+	bgt $t2,0,invalid_loop 
+	beq $s0, 9,  pass 
+	beq $s0, 32, pass 
+	ble $s0, 47, invalid_loop 
+	ble $s0, 57, vaild 
+	ble $s0, 64, invalid_loop 
+	ble $s0, 85, vaild	
+	ble $s0, 96, invalid_loop 
+	ble $s0, 117, vaild 	
+	bge $s0, 118, invalid_loop 
